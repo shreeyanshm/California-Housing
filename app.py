d7db50c54d05fc5cc16185cfeda6183007a4276c
Shreeyansh Mittal <shreeyanshmittal@gmail.com>
2025-06-03 22:42:40 +0530

    app.py modified

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def predict():
     final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+    return render_template("home.html", prediction_text=output)
 
 if __name__=='__main__':
     app.run(debug=True)
